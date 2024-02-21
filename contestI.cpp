a1137904628bb2fe61b5c9bd64a1c58a1d716640
WolfGray1 <160720007+WolfGray1@users.noreply.github.com>
2024-02-21 14:50:27 +0600

    Add files via upload

---
+++ contestI.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    ifstream inputFile("input.txt");
+    ofstream outputFile("output.txt");
+
+    int K;
+    if (inputFile) {
+        inputFile >> K;
+    }
+    else {
+        cin >> K;
+    }
+
+    int dayOfWeek = (3 + K - 2) % 7;
+
+    if (outputFile) {
+        outputFile << dayOfWeek;
+    }
+    else {
+        cout << dayOfWeek;
+    }
+
+    return 0;
+}
